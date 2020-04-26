67b6e8f55586e1158a1af6b6e57fe4cc26594515
Oğuzhan Turan <turan.oguzhan@hotmail.com>
2020-04-26 19:12:41 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,14 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  int currQuestion = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[currQuestion],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,9 +72,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(Icons.check, color: Colors.green),
-                  );
+                  if (questions.length - 1 > currQuestion) {
+                    scoreKeeper.add(
+                      Icon(Icons.check, color: Colors.green),
+                    );
+                    currQuestion++;
+                  }
                 });
               },
             ),
@@ -86,9 +97,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(Icons.close, color: Colors.red),
-                  );
+                  if (questions.length - 1 > currQuestion) {
+                    scoreKeeper.add(
+                      Icon(Icons.close, color: Colors.red),
+                    );
+                    currQuestion++;
+                  }
                 });
               },
             ),
@@ -101,7 +115,7 @@ class _QuizPageState extends State<QuizPage> {
 }
 
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
+question1: false
+question2: true,
+question3: true,
 */
